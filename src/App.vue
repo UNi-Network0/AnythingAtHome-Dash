c4ffeb562650190954447e8af43f5031ab3fb860
unify-z <3336016933@qq.com>
2024-10-06 19:44:21 +0800

    feat: 使用鸿蒙字体

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
+  <a-config-provider
+      :theme="{
+        token: {
+          fontFamily: `HMFont, sans-serif`,
+          fontSize: 15,
+          lineWidth: 0,
+          fontWeightStrong: 900
+        }
+      }"
+    >
   <DefaultLayout>
   </DefaultLayout>
+</a-config-provider>
 </template>
 
 <script setup>
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 </script>
+
+<style>
+@font-face {
+    font-family: 'HMFont';
+    src: url('https://cdn.unifyz.5k.work/HarmonyOS_Sans_Medium.ttf');
+    font-weight: normal;
+    font-style: normal;
+}
+body {
+    font-family: 'HMFont';
+}
+</style>
